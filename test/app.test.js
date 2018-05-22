40bdc2764717e3dfecefc36f925e49e7469a6163
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-05-21 21:10:04 -0500

    Restructuring test hierarchy

--- test/app.test.js
+++ test/app.test.js
@@ -1,33 +1,40 @@
 let faker = require('faker');
 
-const mock = jest.fn();
-jest.mock('socket.io-client', () => {
-  return mock;
-});
-let endpoint = faker.internet.url();
-let socket   = require('socket.io-client');
-let config = jest.mock('../src/config.js', () => {
-  return {
-    endpoint: endpoint
-  };
-});
+describe('app', () => {
+  let app = {};
 
-let app = require('../src/app.js');
-
-describe('start', () => {
-  test('calls socketio client with correct params', () => {
-    let accessToken = faker.random.uuid();
-    return app.start(accessToken)
-      .then(() => {
-        let mockArgs = mock.mock.calls[0];
-        expect(mockArgs[0]).toBe(endpoint);
-
-        // deep equal check
-        expect(mockArgs[1]).toEqual({
-          transports: ['websocket'],
-          path: '/thermostat',
-          extraHeaders: { Authorization: `Bearer ${accessToken}` }
-        });
-      });
+  const mock = jest.fn();
+  jest.mock('socket.io-client', () => {
+    return mock;
   });
+
+  let endpoint = faker.internet.url();
+  let config = jest.mock('../src/config.js', () => {
+    return {
+      endpoint: endpoint
+    };
+  });
+
+  describe('start', () => {
+    beforeEach(() => {
+      app = require('../src/app.js');
+    });
+
+    test('calls socketio client with correct params', () => {
+      let accessToken = faker.random.uuid();
+      return app.start(accessToken)
+        .then(() => {
+          let mockArgs = mock.mock.calls[0];
+          expect(mockArgs[0]).toBe(endpoint);
+
+          // deep equal check
+          expect(mockArgs[1]).toEqual({
+            transports: ['websocket'],
+            path: '/thermostat',
+            extraHeaders: { Authorization: `Bearer ${accessToken}` }
+          });
+        });
+    });
+  });
+
 });
