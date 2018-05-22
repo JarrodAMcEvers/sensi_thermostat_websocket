a754014d3e0ef8f58f7057703ae24dc21d64ae2d
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-05-21 21:09:23 -0500

    /src/index will be the starting point of the program

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let app    = require('./app.js');
+let config = require('./config.js');
+
+app.start(config.access_token);
---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+let faker = require('faker');
+
+describe('index', () => {
+  let index = {};
+  let configToken = faker.random.uuid();
+  let config = jest.mock('../src/config.js', () => {
+    return {
+      access_token: configToken
+    };
+  });
+
+  let startMock = jest.fn();
+  let app = jest.mock('../src/app.js', () => {
+    return {
+      start: startMock
+    };
+  });
+
+  test('calls app.start with access_token from config', () => {
+    index = require('../src/index.js');
+    expect(startMock).toHaveBeenCalledWith(configToken);
+  });
+});
