2cd485fe938812e4fe91f5f5836790957eca88b7
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-06-29 21:06:54 -0500

    Catch errors and exit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 let app    = require('./app.js');
 let config = require('./config.js');
 
-app.startSocketConnection(config.access_token);
+app.startSocketConnection(config.access_token)
+  .catch(err => {
+    console.error('could not setup socket connection', err);
+    process.exit(1);
+  });
