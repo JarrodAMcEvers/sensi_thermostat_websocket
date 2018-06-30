d44906a6a5c4fe979b55412e33453d91ddc4072c
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-06-29 20:51:28 -0500

    Cleanup of the mocks in index

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,15 @@
 let faker = require('faker');
 
 describe('index', () => {
-  let index = {};
-  let configToken = faker.random.uuid();
-  let config = jest.mock('../src/config.js', () => {
-    return {
-      access_token: configToken
-    };
-  });
+  let index;
 
-  let startMock = jest.fn();
-  let app = jest.mock('../src/app.js', () => {
-    return {
-      startSocketConnection: startMock
-    };
-  });
+  let configToken = faker.random.uuid();
+  let config      = { access_token: configToken };
+  let mockConfig  = jest.mock('../src/config.js', () => config);
+
+  let startMock = jest.fn(() => Promise.resolve());
+  let app       = { startSocketConnection: startMock };
+  let appMock   = jest.mock('../src/app.js', () => app);
 
   test('calls app.start with access_token from config', () => {
     index = require('../src/index.js');
