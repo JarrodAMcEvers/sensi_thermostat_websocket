b94ff28447596e5b445d4c98061fb988d5241a33
Jarrod McEvers <jarrodamcevers@gmail.com>
2018-07-08 20:18:36 -0500

    Reformat

--- src/authorization.js
+++ src/authorization.js
@@ -5,18 +5,20 @@ let util    = require('util');
 let post = util.promisify(request.post);
 
 exports.getAccessToken = async () => {
-  return await post(`${config.token_endpoint}/token`, { form: {
-    grant_type: 'password',
-    client_id: config.client_id,
-    client_secret: config.client_secret,
-    username: config.username,
-    password: config.password
-  }})
+  return await post(`${config.token_endpoint}/token`, {
+    form: {
+      grant_type: 'password',
+      client_id: config.client_id,
+      client_secret: config.client_secret,
+      username: config.username,
+      password: config.password
+    }
+  })
     .then(res => {
       let body = JSON.parse(res.body);
       if (res.statusCode === 400) {
         return Promise.reject(body);
       }
       return body.access_token;
-  });
+    });
 };
