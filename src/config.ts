06df5b375e54a917f9b825c08a2b0d4dab78ab77
Jarrod McEvers <jarrodamcevers@gmail.com>
2020-01-14 07:53:46 -0600

    I prefer uppercase for process env variables.

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const socket_endpoint = 'https://rt.sensiapi.io';
 export const token_endpoint  = 'https://oauth.sensiapi.io';
-export const client_id       = process.env.client_id;
-export const client_secret   = process.env.client_secret;
-export const username        = process.env.username;
-export const password        = process.env.password;
+export const client_id       = process.env.CLIENT_ID;
+export const client_secret   = process.env.CLIENT_SECRET;
+export const username        = process.env.USERNAME;
+export const password        = process.env.PASSWORD;
