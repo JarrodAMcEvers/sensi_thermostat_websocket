6cc1fc501b907d00b59bc7786349873ded128eeb
Jarrod McEvers <jarrodamcevers@gmail.com>
2020-01-21 08:55:03 -0600

    Forgot to remove on listeners in app. This is being done in socket.ts

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,4 @@ export async function startSocketConnection(): Promise<void> {
   const socket           = new Socket(accessToken);
   const socketConnection = socket.connection;
 
-  socketConnection.on('connected', this.connectHandler);
-  socketConnection.on('disconnect', this.disconnectHandler);
-  socketConnection.on('error', this.errorHandler);
 }
