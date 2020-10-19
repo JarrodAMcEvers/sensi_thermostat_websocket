10af44ea9cb67d1daa2e7459fad8b198e87ed3fd
Jarrod McEvers <jarrod.mcevers@centene.com>
2020-10-19 10:49:09 -0500

    Get rid of Tokens interface. Add some typing. Export all interfaces.

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,7 @@
+type Epoch = number;
+type FleetEnabled = true | false;
+type DisplayScale = 'f' | 'c';
+
 export interface SensiOAuthResponse {
   access_token: string;
   refresh_token: string;
@@ -11,11 +15,46 @@ export interface SensiOAuthResponse {
   eula_accepted: boolean;
   alerts: boolean;
   offers: boolean;
-  fleet_enabled: boolean;
+  fleet_enabled: FleetEnabled;
   fleet_billing_option: string;
 }
 
-export interface Tokens {
-  access_token: string;
-  refresh_token: string;
+export interface Control {}
+
+export interface DemandStatus {
+  last_start?: Epoch;
+  heat: number;
+  cool: number;
+  fan: number;
+  heat_stage?: number;
+}
+
+export interface State {
+  display_temp?: number;
+  display_scale?: DisplayScale;
+  humidity?: number;
+  control: Control;
+  demand_status?: DemandStatus
+}
+
+export interface Registration {
+  name: string;
+  city: string;
+  state: string;
+  country: string;
+  address1: string;
+  address2?: string;
+  timezone: string;
+  postal_code: string;
+  product_type: string;
+  // contractor id might be an int
+  contractor_id?: string;
+  fleet_enabled: FleetEnabled;
+  fleet_enabled_date?: string;
+}
+
+export interface SocketState {
+  icd_id: string;
+  registration: Registration;
+  state: State;
 }
