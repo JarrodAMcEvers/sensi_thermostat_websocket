fe96d88e1f1ef26b9a0b829f7ffdb4fec6378637
Jarrod McEvers <jarrod.mcevers@rgare.com>
2021-08-11 08:28:15 -0500

    Add logging.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { Authorization } from './authorization';
 import { Socket } from './socket/socket';
 
-const authorization    = new Authorization();
-const socket           = new Socket(authorization);
+const authorization = new Authorization();
+const socket        = new Socket(authorization);
+
+console.log('Starting socket connection with Sensi');
 socket.startSocketConnection()
   .catch(err => {
     console.error('could not setup socket connection', err);
