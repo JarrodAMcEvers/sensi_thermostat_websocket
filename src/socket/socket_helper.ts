582717ee0aa577b5bb1fe685c1f4ef6cc9d31d0e
asdfjkalsdfla <asdfjkalsdfla@asdfjkalsdfla.com>
2022-04-10 16:58:39 -0400

    fix warnings/errors

--- src/socket/socket_helper.ts
+++ src/socket/socket_helper.ts
@@ -1,14 +1,12 @@
-import { SocketState } from "../types/types";
-
 export class SocketHelper {
   static stateHandler(data: any): void {
-    const datetime = new Date().toLocaleString().replace(",", "");
+    const datetime = new Date().toLocaleString().replace(',', '');
 
-    console.log("received data at", datetime);
+    console.log('received data at', datetime);
     console.log(JSON.stringify(data));
   }
 
   static disconnectHandler(err): void {
-    console.error("disconnected", err);
+    console.error('disconnected', err);
   }
 }
