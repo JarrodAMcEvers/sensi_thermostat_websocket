d5859b067cb5db161aee5b0f584e7e071d301dbd
Tom Thumb <github@asdfjkalsdfla.com>
2022-04-10 17:28:02 -0400

    resolves socket helper test failures

--- test/socket/socket_helper.test.ts
+++ test/socket/socket_helper.test.ts
@@ -1,45 +1,17 @@
-import * as faker from 'faker';
-
-const socketObject = {
-  on: jest.fn()
-};
-const mockSocket = jest.fn(() => socketObject);
-jest.mock('socket.io-client', () => mockSocket);
-
-const mockEndpoint = faker.internet.url();
-const mockConfig = {
-  socketEndpoint: mockEndpoint
-};
-jest.mock('../../src/config', () => mockConfig);
-
-const socketConnection = {
-  on: jest.fn()
-};
-const mockSocketObject = {
-  startSocketConnection: jest.fn(() => socketConnection)
-};
-const socket = jest.fn().mockImplementation(() => mockSocketObject);
-jest.mock('../../src/socket/socket', () => ({ Socket: socket }));
-
 import { SocketHelper } from '../../src/socket/socket_helper';
 
 describe('socket_helper', () => {
-  let testSocketHelper;
-  beforeEach(() => {
-    testSocketHelper = new SocketHelper();
-  });
-
   describe('stateHandler', () => {
     // placeholder test for now
     // jest complains if a describe block does not have at least one test.
     test('sets data on message', async () => {
+      const logSpy = jest.spyOn(console, 'log');
       const data = {
         data: 'one',
         other: 'two'
       };
-      await testSocketHelper.stateHandler(data);
-
-      expect(testSocketHelper.state).toEqual(data);
+      SocketHelper.stateHandler(data);
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(JSON.stringify(data)));
     });
   });
 });
